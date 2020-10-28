625fce4fac30cc6bf19e119a2ec2ca8cb179e3c3
Sofia Dutta <sofiad1@umbc.edu>
2020-10-27 21:48:58 -0400

    modified:   split-join.sh

--- Data/split-join.sh
+++ Data/split-join.sh
@@ -5,9 +5,11 @@ else
 	if [ $1 == "j" ]; then
 		echo "Joining"
 		cat dev_v2.1.json.gz.part* > dev_v2.1.json.gz
+		cat twcs.csv.part-a* > twcs.csv
 	elif [ $1 == "s" ]; then
 		echo "Splitting dev_v2.1.json.gz"
 		split -b 99m dev_v2.1.json.gz dev_v2.1.json.gz.part-
+		split -l 500000 twcs.csv twcs.csv.part-
 	else
 		echo "Unknown input parameters"
 	fi
